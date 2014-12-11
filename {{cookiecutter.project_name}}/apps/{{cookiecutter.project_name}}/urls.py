191b79bc5202d44f719e02cb623e760a2d06ca2c
Yuri Shevchuk <yuri.shevchuk@mediasapiens.co>
2014-12-11 23:52:35 +0200

    Cookiecutter Django 1.7 project base template.

---
+++ {{cookiecutter.project_name}}/apps/{{cookiecutter.project_name}}/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.conf import settings
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns(
+        '',
+        url(r'^debug/', include(debug_toolbar.urls)),
+    )
---
+++ {{cookiecutter.project_name}}/settings/development.py
@@ -0,0 +1,15 @@
+from base import *
+
+
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
+INSTALLED_APPS += (
+    'debug_toolbar',
+    'django_extensions',
+)
+
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INTERNAL_IPS = ('127.0.0.1',)
